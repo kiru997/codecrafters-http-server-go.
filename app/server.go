b322a3649af50a63c5e777eb9e06dcf5f3dcbfd2
Dang Nguyen <dang.nguyen@deeptech.com.vn>
2024-04-22 20:36:09 +0700

    pass stage

--- app/server.go
+++ app/server.go
@@ -4,10 +4,13 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 	// Uncomment this block to pass the first stage
 	// "net"
 	// "os"
 )
+
+var CRLF = []byte("\r\n")
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -21,15 +24,60 @@ func main() {
 		os.Exit(1)
 	}
 
-	conn, err := l.Accept()
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		err = handle(conn)
+		if err != nil {
+			fmt.Println("Error handle connection: ", err.Error())
+			continue
+		}
+
+	}
+}
+
+func parse(buf []byte) []string {
+	str := string(buf)
+	strs := strings.Split(str, string(CRLF))
+	return strs
+}
+
+func getPath(str string) string {
+	paths := strings.Split(str, " ")
+	if len(paths) < 2 {
+		return ""
+	}
+	return paths[1]
+}
+
+func handle(conn net.Conn) error {
+	defer conn.Close()
+
+	buf := make([]byte, 1024)
+	_, err := conn.Read(buf)
 	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+		fmt.Println("Failed to bind to port 4221")
+		return err
 	}
 
-	_, err = conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	strs := parse(buf)
+	path := getPath(strs[0])
+	if path == "/" {
+		_, err = conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+		if err != nil {
+			fmt.Println("Error Write connection: ", err.Error())
+			return err
+		}
+		return nil
+	}
+
+	_, err = conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
 	if err != nil {
 		fmt.Println("Error Write connection: ", err.Error())
-		os.Exit(1)
+		return err
 	}
+	return nil
 }
